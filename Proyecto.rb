fc52a9ed3f669dce603dcf21f765812a74cdaf40
Dennys <denisaguilon@gmail.com>
2018-10-24 21:56:20 -0600

    Archivo de Gemas y Implementacion de funciones en el Proyecto

--- Proyecto.rb
+++ Proyecto.rb
@@ -3,8 +3,6 @@ require 'terminal-table'
 
 def limpiar_pantalla
     system('clear')
-    #En caso de que no limpie la pantalla comenten la anterior linea y des comenten la siguiente
-    #system('cls')
 end
 def novalido
     puts "LA OPCION INGRESADA NO EXISTE"
@@ -27,6 +25,285 @@ pila = {
   size: 0
 }
 
+def librosporautor(nombre,pila)#complemento
+  libro = pila[:tope]
+  conta=1
+  unidad=0
+  while conta<=pila[:size]
+    if libro[:autor]==nombre
+      unidad+=1
+    end
+
+    if  libro[:siguiente]==nil
+      conta=pila[:size]+1
+
+    else
+      nuevo_elemento = libro[:siguiente]
+      libro = nuevo_elemento
+      conta+=1
+    end
+  end
+  return unidad
+end
+# cant Unidades
+def ver_unidades(pila,isbn)
+  libro = pila[:tope]
+  conta=1
+  unidad=0
+  while conta<=pila[:size]
+    if libro[:ISBN]==isbn
+      unidad+=1
+    end
+
+    if  libro[:siguiente]==nil
+      conta=pila[:size]+1
+
+    else
+      nuevo_elemento = libro[:siguiente]
+      libro = nuevo_elemento
+      conta+=1
+    end
+  end
+  return unidad
+end
+
+#BUSCAR AUTOR
+def buscar_autor(cola,nombre)
+  elemento = cola[:tope]
+  conta=1
+  while conta<=cola[:size]
+    if elemento[:nombre]==nombre
+      conta=6
+      return elemento
+      break
+    elsif   elemento[:siguiente]==nil && elemento[:valor]!=nombre
+      return '***El autor no existe en el sistema***'
+      break
+    else
+      nuevo_elemento = elemento[:siguiente]
+      elemento = nuevo_elemento
+      conta+=1
+    end
+  end
+end
+# Mostrar autor
+def mostrar_autor(cola,pila)
+  if cola[:max]==5
+    puts 'no hay autores para mostrar'
+  else
+    print 'Ingrese el nombre del autor: '
+    nombre = gets.chomp
+    elemento = buscar_autor(cola,nombre)
+    if elemento=='***El autor no existe en el sistema***'
+      puts elemento
+    else
+      if elemento[:libros]==0#cambio
+        tabla = Terminal::Table.new do |t|
+        t.title = "Nombre Autor: #{elemento[:nombre]}"
+        t.add_row(['El autor no tiene ningun libro'])
+      end
+        puts tabla
+      else
+        tope = pila[:tope]
+        tabla = Terminal::Table.new do |t|
+        t.title = " Autor -#{elemento[:nombre]}-"
+        t.headings=(['ISBN','NOMBRE','PRECIO','Unidades'])
+    loop do
+      if tope[:autor] == elemento[:nombre] && tope[:se_imprime] != 'no'
+      t.add_row([
+        tope[:ISBN],
+        tope[:nombre],
+        tope[:precio],
+        ver_unidades(pila,tope[:ISBN])
+
+      ])
+      if tope[:siguiente] == nil
+        break
+      end
+      tope = tope[:siguiente]
+    else
+    if tope[:siguiente] == nil
+      break
+    end
+    tope = tope[:siguiente]
+  end
+      end
+
+        end
+        puts tabla
+      end
+
+    end
+  end
+    
+end
+
+#Registro de autor
+def nuevo_autor(cola)
+  if cola[:max]>0
+    if cola[:esta_vacio]            #Verifica si el Autor existe en la Pila
+      print 'Ingrese el nombre del autor: '
+      nombre_autor = gets.chomp
+      autor = {
+        nombre:nombre_autor,
+        libros: 0,
+        siguiente:nil,#cambio
+      }
+      cola[:tope] = autor
+      cola[:final] = autor
+      cola[:esta_vacio] = false
+      cola[:max]-=1
+      cola[:size]+=1
+    else                                      #si ya existe mas de algun autor
+      print 'Ingrese el nombre del autor: '
+      nombre_autor = gets.chomp
+      elemento = cola[:tope]
+      conta=1
+      b=0
+      while conta<=cola[:size]                #Verifica si hay autores iguales
+        if elemento[:nombre]==nombre_autor
+          b+=1
+        end
+        if conta!=cola[:size]
+        nuevo_elemento = elemento[:siguiente]
+        elemento = nuevo_elemento
+        end
+      conta+=1
+
+    end
+    if b>0                                 #Cuando se ingresa el mismo autor
+      puts '***ya existe un autor con este nombre***'
+    else                                      #cuando no existe crea uno nuevo autor
+      autor = {
+        nombre:nombre_autor,
+        libros: 0,
+        libros1: nil,
+        esta_vacio: true,
+        siguiente:nil,
+      }
+      a = cola[:final]
+      a[:siguiente] = autor
+      cola[:final] = autor
+      cola[:max]-=1
+      cola[:size]+=1
+    end
+    end
+  else
+    puts "\n***Ya no tiene espacio para mas autores***"
+
+  end
+end
+# Lista de autores
+def lista_de_autores(cola,pila)
+  if cola[:max]==5
+    puts "***No hay autores en el sistema***"
+  else
+    tabla = Terminal::Table.new do |t|
+    t.title = 'Lista De Autores'
+    t.headings = ['Nombre', 'Libros']
+
+    aux = cola[:tope]
+    loop do
+      siguiente = aux[:siguiente]
+      t.add_row([
+        aux[:nombre],
+        librosporautor(aux[:nombre],pila)
+      ])
+      if aux[:siguiente] == nil
+        break
+      end
+      aux = aux[:siguiente]
+    end
+  end
+  puts tabla
+  end
+end
+# buscar libro 1
+def buscar_libro(pila,isbn)
+  libro = pila[:tope] # cambio de puntero
+  conta=1
+  while conta<=pila[:size]
+    if libro[:ISBN]==isbn
+      conta=pila[:size]+1
+      return libro
+    elsif   libro[:siguiente]==nil && libro[:ISBN]!=isbn
+      return 'no existe el libro'
+      break
+    else
+      nuevo_elemento = libro[:siguiente]
+      libro = nuevo_elemento
+      conta+=1
+    end
+  end
+end
+
+#lista de libros
+def lista_libros(pila)
+  if pila[:tope] == nil
+    puts 'No hay libros en el sistema'
+  else
+  tabla = Terminal::Table.new do |t|
+    t.title = 'LISTA DE LIBROS'
+    t.headings = ['ISBN', 'Nombre', 'Precio','Autor','Unidades']
+
+    aux = pila[:tope]
+
+    loop do
+      if aux[:se_imprime]!='no'
+        t.add_row([
+          aux[:ISBN],
+          aux[:nombre],
+          aux[:precio],
+          aux[:autor],
+          ver_unidades(pila,aux[:ISBN])])
+    end
+      if aux[:siguiente] == nil
+        break
+      end
+      aux = aux[:siguiente]
+    end
+  end
+
+  puts tabla
+end
+end
+#BUSCAR UN LIBRO
+def buscar_libro1(pila)
+  libro = pila[:tope]
+  print 'Ingrese el ISBN: '
+  isbn = gets.chomp
+  conta=1
+  b=0
+  while conta<=pila[:size]
+    if isbn == libro[:ISBN]
+      conta=pila[:size]+1
+      b=1
+    elsif libro[:siguiente]==nil&&libro[:ISBN]!=isbn
+      b=0
+    else
+      libro = libro[:siguiente]
+    end
+    conta+=1
+  end
+  if b==1
+    limpiar_pantalla
+    tabla = Terminal::Table.new do |t|
+      t.headings = ['Nombre','unidades','ISBN','Precio','Unidades']
+      t.add_row([
+        libro[:nombre],
+        libro[:autor],
+        libro[:ISBN],
+        libro[:precio],
+        ver_unidades(pila,libro[:ISBN])
+      ])
+    end
+    puts tabla
+  else
+    limpiar_pantalla
+    puts 'El libro que busca no existe en el sistema'
+  end
+
+end
 #Registro de Nuevo libro
 def Reg_New_libro(pila,cola)
     if cola[:esta_vacio]
@@ -103,13 +380,13 @@ def Reg_New_libro(pila,cola)
                 puts "Ingrese un ISBN DIFERENTE!!!"
               end
   
-            end#fin del if si se encuntra un isbn igual al ingresado. se busca en el nodo autor
+            end
   
-          end#fin de ingresar un libro en un autor sin libro
-      end#fin condicion busqueda del autor
-    end#fin primer if
-    #gets
-  end#fin de la funcion
+          end
+      end
+    end
+    
+  end
 
 
 #Menu 3 de Administracion de Libros
@@ -137,26 +414,31 @@ def menuadmin(pila,cola)
             limpiar_pantalla
         when '2'  
             puts"Reg. de Autores"
+            nuevo_autor(cola)
             #Limpiar pantalla y una pausa
             gets()
             limpiar_pantalla
         when '3'  
             puts"Listado de Libros"
+            buscar_libro1(pila)
             #Limpiar pantalla y una pausa
             gets()
             limpiar_pantalla
         when '4' 
             puts"Listado de Autores"
+            mostrar_autor(cola,pila)
             #Limpiar pantalla y una pausa
             gets()
             limpiar_pantalla
         when '5' 
             puts"Buscar Libro"
+            lista_de_autores(cola,pila)
             #Limpiar pantalla y una pausa
             gets()
             limpiar_pantalla
         when '6' 
             puts"Buscar Autor"
+            lista_libros(pila)
             #Limpiar pantalla y una pausa
             gets()
             limpiar_pantalla
